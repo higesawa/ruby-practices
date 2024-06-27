ca799519fbf23095c6055c224f35a868284cdbe9
higesawa <C.yaoce2.5@gmail.com>
2024-06-28 00:20:53 +0900

    レビュー内容の反映と各処理をメソッド化

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,39 +1,63 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-score = ARGV[0]
+def parse_frames(score)
+  scores = score.split(',')
+  shots = []
+  scores.each do |s|
+    if s == 'X'
+      shots << 10
+      shots << 0
+    else
+      shots << s.to_i
+    end
+  end
 
-scores = score.split(',')
-shots = []
-scores.each do |s|
-  if s == 'X'
-    shots << 10
-    shots << 0
+  frames = shots.each_slice(2).to_a
+  frames.map! { |frame| frame == [10, 0] ? [10] : frame }
+  until frames.length == 10
+    last_frame = frames.pop
+    frames[-1] += last_frame
+  end
+  frames
+end
+
+def strike?(frame)
+  frame == [10]
+end
+
+def strike_score(next_frame, next_next_frame)
+  if strike?(next_frame)
+    10 + next_frame[0] + next_next_frame[0]
   else
-    shots << s.to_i
+    10 + next_frame[0..1].sum
   end
 end
 
-frames = []
-shots.each_slice(2) { |s| frames << s }
-frames.map! { |frame| frame == [10, 0] ? [10] : frame }
-until frames.length == 10
-  last_frame = frames.pop
-  frames[-1] += last_frame
+def spare?(frame)
+  frame.sum == 10 && frame.length == 2
 end
 
+def spare_score(next_frame)
+  10 + next_frame[0]
+end
+
+def frame_point(frames, frame, index)
+  next_frame = frames[index + 1]
+  next_next_frame = frames[index + 2]
+  if strike?(frame)
+    strike_score(next_frame, next_next_frame)
+  elsif spare?(frame)
+    spare_score(next_frame)
+  else
+    frame.sum
+  end
+end
+
+score = ARGV[0]
+frames = parse_frames(score)
 point = 0
 frames.each_with_index do |frame, i|
-  point += if frame == [10]
-             if frames[i + 1] == [10]
-               frame.sum + frames[i + 1][0] + frames[i + 2][0]
-             else
-               frame.sum + frames[i + 1][0] + frames[i + 1][1]
-             end
-           elsif frame.sum == 10 && frame.length == 2
-             frame.sum + frames[i + 1][0]
-           else
-             frame.sum
-           end
+  point += frame_point(frames, frame, i)
 end
 puts point
